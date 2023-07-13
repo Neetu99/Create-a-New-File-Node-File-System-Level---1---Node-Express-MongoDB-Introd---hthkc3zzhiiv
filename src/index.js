35c3d90f03b3b5a94a749f01880c2c776747288b
Neetu Ojha <120268912+Neetu99@users.noreply.github.com>
2023-07-13 18:04:47 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const fileContent = "Newton School";
 const writeFile = async (fileName, fileContent) => {
   // write code here
   // dont change function name
+  fs.writeFile(fileName,fileContent,(error)=>{
+    console.log(error);
+  });
 };
 
 module.exports = { writeFile };
